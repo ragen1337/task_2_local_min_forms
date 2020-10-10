27c4273e5a5322185c203ceb70cccc46073cbf19
ragen1337 <rabgospodanashego@gmail.com>
2020-10-10 21:28:48 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/ArrayHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class ArrayHandler
+    {
+        public static int[] RandomArrayGenerator(int Amount)
+        {
+            Random random = new Random();
+            int[] array = new int[Amount];
+            int i = 0;
+            foreach (int elem in array)
+            {
+                array[i] = random.Next(1, 50);
+                Console.WriteLine(i + ") " + array[i]);
+                i++;
+            }
+
+            return array;
+        }
+
+        public static int LocalMinimumAmount(int[] array)
+        {
+            int amount = 0;
+            int array_length = array.Length;
+
+            if (array[0] < array[1])
+                amount++;
+            if (array[array_length - 1] < array[array_length - 2])
+                amount++;
+
+            for (int i = 1; i < array_length - 1; i++)
+            {
+                if (array[i] < array[i - 1] && array[i] < array[i + 1])
+                {
+                    amount++;
+                    i++;
+                }
+            }
+
+            return amount;
+        }
+    }
+}
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int N;
+            try
+            {
+                N = int.Parse(textBox1.Text);
+
+                if (N < 3)
+                {
+                    throw new Exception();
+                }
+            }
+            catch
+            {
+                N = 10;
+                textBox1.Text = "10";
+            }
+
+            int[] array = ArrayHandler.RandomArrayGenerator(N);
+            string output_text = "";
+            int i = 0;
+            foreach(int elem in array)
+            {
+                output_text += array[i] + " ";
+                i++;
+            }
+
+            textBox2.Text = output_text;
+
+            int LocalMinimumAmount = ArrayHandler.LocalMinimumAmount(array);
+
+            label4.Text = "" + LocalMinimumAmount;
+        }
+    }
+}
